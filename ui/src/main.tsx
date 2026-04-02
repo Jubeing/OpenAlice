029a7bc02c40de8dec810427706ad69509ecebbe
Jubeing <128659352+Jubeing@users.noreply.github.com>
2026-04-02 15:42:40 +0800

    Update main.tsx

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -3,14 +3,17 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { App } from './App'
 import { ToastProvider } from './components/Toast'
+import { LanguageProvider } from './contexts/LanguageContext'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <ToastProvider>
-        <App />
-      </ToastProvider>
+      <LanguageProvider>
+        <ToastProvider>
+          <App />
+        </ToastProvider>
+      </LanguageProvider>  
     </BrowserRouter>
   </StrictMode>,
 )
